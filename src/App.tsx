8b9ec104814307cdbc630e552e39bdadc7ceb947
Henry Milac <henrymilacprog@gmail.com>
2023-11-19 16:10:04 -0500

    Can edit a person

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <>
 
-      {/* <FormNewEditDelete/> */}
+      <FormNewEditDelete/>
       {/* <CreateUseFetch/> */}
       {/* <Sidebar/> */}
       {/* <IconMenuToNavbar/> */}
--- src/Mixer/Exercises/FormNewEditDelete/FormNewEditDelete.tsx
+++ src/Mixer/Exercises/FormNewEditDelete/FormNewEditDelete.tsx
@@ -1,50 +1,68 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function FormNewEditDelete() {
 
     const [persons, setPersons] = useState([])
+    const [person, setPerson] = useState({})
     const [name, setName] = useState('')
     const [age, setAge] = useState('')
     
     const handleSubmit = (e) => {
         e.preventDefault()
-        
-        const person = {
-            id: Date.now(),
+
+        const personObject = {
             name, 
             age
         }
 
-        setPersons([...persons, person])
+        if(person.id){
+            personObject.id = person.id
+            const personUpdate = persons.map(
+                personState => personState.id === person.id ? personObject : personState
+            )
+            setPersons(personUpdate)
+            setPerson({})
+        }else {
+            personObject.id = Date.now()
+            setPersons([...persons, personObject])
+        }
+      
         setName('')
         setAge('')
     }
+
+    useEffect(() => {
+        if(Object.keys(person).length > 0){
+            setName(person.name)
+            setAge(person.age)
+        }
+    }, [person])
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="">Name: </label>
-                    <input type="text" placeholder="Name" className="text-black"
+                    <input type="text" placeholder="Name" className="border"
                         value={name}
                         onChange={(e) => {setName(e.target.value)}}
                     />
                 </div>
                 <div>
                     <label htmlFor="">Age: </label>
-                    <input type="number" placeholder="Age" className="text-black"
+                    <input type="number" placeholder="Age" className="border"
                         value={age}
                         onChange={(e) => {setAge(e.target.value)}}
                     />
                 </div>
-                <input type="submit" value="Send" />
+                <input type="submit" value={person.id ? 'Edit': 'Send'} />
             </form>
             {persons.map((person) => (
                 <div className="flex justify-between" key={person.id}>
                     <p>{person.name}</p>
                     <p>{person.age}</p>
                     <div className="">
-                        <button>Edit</button>
+                        <button onClick={() => setPerson(person)}>Edit</button>
                         <button>Delete</button>
                     </div>
                 </div>
