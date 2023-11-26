28d39456eb4d062a370cfe6b712852b21454d001
Henry Milac <henrymilacprog@gmail.com>
2023-11-26 14:39:10 -0500

    Some details

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import IconMenuToNavbar from "./Mixer/Components/IconMenuToNavbar/IconMenuToNavbar";
-import Sidebar from "./Mixer/Components/Sidebar/Sidebar";
-import DarkModeTailwind from "./Mixer/Exercises/DarkModeTailwind/DarkModeTailwind";
+import IconMenuToNavbar from "./Mixer/Components/IconMenuToNavbar";
+import Sidebar from "./Mixer/Components/SideBar";
+import DarkModeTailwind from "./Mixer/Exercises/DarkModeTailwind";
 import AxiosConsuming from "./Mixer/Exercises/Fetching/AxiosConsuming/AxiosConsuming";
 import CreateUseFetch from "./Mixer/Exercises/Fetching/CreateUseFetch/CreateUseFetch";
-import FormNewEditDelete from "./Mixer/Exercises/FormNewEditDelete/FormNewEditDelete";
+import FormNewEditDelete from "./Mixer/Exercises/FormNewEditDelete";
 import Z from "./Mixer/Z/Z";
 
 export default function App() {
