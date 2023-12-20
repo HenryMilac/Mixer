408020c822b381c1edd31bc61bac2a261fb29a44
Henry Milac <henrymilacprog@gmail.com>
2023-12-19 20:24:15 -0500

    Zustan Fernando Herrera cap2 done

--- src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera.tsx
+++ src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera.tsx
@@ -1,39 +1,39 @@
+import { useShallow } from "zustand/react/shallow"
 import { count } from "./store/notes.store"
-
 
 export default function ZustandFernandoHerrera() {
 
-    const Products = count(state => state.products)
+    const Products = count(useShallow(state => state.products))
     const CountProduct1 = count(state => state.countProduct1)
     const CountProduct2 = count(state => state.countProduct2)
+    const TotalProducts = count(state => state.totalProducts.totalProductsCount)
     const PlusCountProduct1 = count(state => state.plusCountProduct1)
     const RestCountProduct1 = count(state => state.restCountProduct1)
     const PlusCountProduct2 = count(state => state.plusCountProduct2)
     const RestCountProduct2 = count(state => state.restCountProduct2)
+    const DoNothing = count(state => state.doNothing)
+    const ClearProducts = count(state => state.clearProducts)
+    const AddProduct = count(state => state.addProduct)
 
     return (
         <div className="p-5 flex flex-col gap-10">
             <div>
-                <p>Products (Map from array):</p>
-                <hr/>
-                {Products.map(product => (
-                    <p>{product.name}</p>
-                ))}
+                <h1 className="text-xl text-red-600">Section 01:</h1>
+                <hr />
             </div>
-
-
+            
             <div>
                 <p>Update state function:</p>
                 <hr/>
                 <div className="flex justify-between">
-                    <p>Product 01: {CountProduct1}</p>
+                    <p>Product 01: {CountProduct1} units</p>
                     <div>
                         <button onClick={RestCountProduct1}>Rest</button>
                         <button onClick={PlusCountProduct1}>Plus</button>
                     </div>
                 </div>
                 <div className="flex justify-between">
-                    <p>Product 02: {CountProduct2}</p>
+                    <p>Product 02: {CountProduct2} units</p>
                     <div>
                         <button onClick={RestCountProduct2}>Rest</button>
                         <button onClick={PlusCountProduct2}>Plus</button>
@@ -42,13 +42,34 @@ export default function ZustandFernandoHerrera() {
             </div>
 
 
+
+            <div>
+                <p>Total Products:</p>
+                <hr />
+                <p>Counter: {TotalProducts}</p>
+            </div>
+
             <div>
                 <p>Buttons:</p>
-                <hr />
-                <button>Do nothing</button>
-                <button></button>
-                <button>Clean</button>
+                <hr/>
+                <div className="flex justify-around">
+                    <button onClick={DoNothing}>Do nothing</button>
+                    <button onClick={ClearProducts}>Clear Products</button>
+                    <button onClick={AddProduct}>Add Product</button>
+                </div>
             </div>
+
+
+
+            <div>
+                <p>Products (Map from array):</p>
+                <hr/>
+                {Products.map(product => (
+                    <p key={product.id}>{product.name}</p>
+                ))}
+            </div>
+
+
 
         </div>
     )
--- src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/store/notes.store.tsx
+++ src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/store/notes.store.tsx
@@ -4,33 +4,49 @@ interface Count {
     products: products[]
     countProduct1: number
     countProduct2: number
+    totalProducts: {totalProductsCount: number}
     plusCountProduct1: () => void
     restCountProduct1: () => void
     plusCountProduct2: () => void
     restCountProduct2: () => void
+    doNothing: () => void
+    clearProducts: () => void
+    addProduct: () => void
 }
 interface products {
     id: number
     name: string
 }
 
-export const count = create<Count>(set => ({
+export const count = create<Count>((set, get) => ({
     products: [
         {id: 1, name: 'Product 01'},
         {id: 2, name: 'Product 02'},
     ],
-    countProduct1: 0,
-    countProduct2: 0,
-    plusCountProduct1: () => set(state => ({
-        countProduct1: state.countProduct1 + 1
-    })),
-    restCountProduct1: () => set(state => ({
-        countProduct1: state.countProduct1 - 1
-    })),
-    plusCountProduct2: () => set(state => ({
-        countProduct2: state.countProduct2 + 1
-    })),
-    restCountProduct2: () => set(state => ({
-        countProduct2: state.countProduct2 - 1
-    })),
+    countProduct1: 2,
+    countProduct2: 3,
+    totalProducts: {get totalProductsCount() {
+        return get().countProduct1 + get().countProduct2
+    }},
+    plusCountProduct1: () => set(state => (
+        {countProduct1: state.countProduct1 + 1}
+    )),
+    restCountProduct1: () => set(state => (
+        {countProduct1: state.countProduct1 - 1}
+    )),
+    plusCountProduct2: () => set(state => (
+        {countProduct2: state.countProduct2 + 1}
+    )),
+    restCountProduct2: () => set(state => (
+        {countProduct2: state.countProduct2 - 1}
+    )),
+    doNothing: () => set(state => (
+        {products: [...state.products]}
+    )),
+    clearProducts: () => set(() => (
+        {products: []}
+    )),
+    addProduct: () => set(state => (
+        {products: [...state.products, {id: state.products.length + 1 , name: `Product 0${state.products.length + 1}`}]}
+    )),
 }))
