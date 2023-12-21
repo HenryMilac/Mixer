a8afae90c85dd9f63417da8a6b36bbf00a182078
Henry Milac <henrymilacprog@gmail.com>
2023-12-21 13:12:20 -0500

    Function plus product reutilizable done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import ZustandFernandoHerrera from "./Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera";
+import ZustandMe from "./Mixer/Issues/ManageState/Zustand/ZustandMe/ZustandMe";
 
 export default function App() {
 
   return (
     <>
       {/* <ZustandFazt/> */}
-      <ZustandFernandoHerrera/>
-
+      <ZustandMe/>
+      {/* <ZustandFernandoHerrera/> */}
 
       {/* <Z/> */}
     </>
