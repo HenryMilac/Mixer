67ac78d3c712a10a5756753cbf86e479c0223ad9
Henry Milac <henrymilacprog@gmail.com>
2023-12-22 13:13:57 -0500

    Advanced

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <>
       {/* <ZustandFazt/> */}
-      <ZustandMe/>
-      {/* <ZustandFernandoHerrera/> */}
+      {/* <ZustandMe/> */}
+      <ZustandFernandoHerrera/>
 
       {/* <Z/> */}
     </>
--- src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera.tsx
+++ src/Mixer/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera.tsx
@@ -1,5 +1,5 @@
 import { useShallow } from "zustand/react/shallow"
-import { count } from "./store/notes.store"
+import { count } from "./store/products.store"
 
 export default function ZustandFernandoHerrera() {
 
@@ -16,61 +16,92 @@ export default function ZustandFernandoHerrera() {
     const AddProduct = count(state => state.addProduct)
 
     return (
-        <div className="p-5 flex flex-col gap-10">
-            <div>
-                <h1 className="text-xl text-red-600">Section 01:</h1>
-                <hr />
-            </div>
+        <div className="p-5 flex flex-col gap-20">
+
+
+
+
+
             
-            <div>
-                <p>Update state function:</p>
-                <hr/>
-                <div className="flex justify-between">
-                    <p>Product 01: {CountProduct1} units</p>
-                    <div>
-                        <button onClick={RestCountProduct1}>Rest</button>
-                        <button onClick={PlusCountProduct1}>Plus</button>
+            {/* ---------- Section 01 */}
+            {/* <div className="flex flex-col gap-10">
+                <div>
+                    <h1 className="text-xl text-red-600">Section 01:</h1>
+                    <hr />
+                </div>
+                
+                <div>
+                    <p>Update state function:</p>
+                    <hr/>
+                    <div className="flex justify-between">
+                        <p>Product 01: {CountProduct1} units</p>
+                        <div>
+                            <button onClick={RestCountProduct1}>Rest</button>
+                            <button onClick={PlusCountProduct1}>Plus</button>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <p>Product 02: {CountProduct2} units</p>
+                        <div>
+                            <button onClick={RestCountProduct2}>Rest</button>
+                            <button onClick={PlusCountProduct2}>Plus</button>
+                        </div>
                     </div>
                 </div>
-                <div className="flex justify-between">
-                    <p>Product 02: {CountProduct2} units</p>
-                    <div>
-                        <button onClick={RestCountProduct2}>Rest</button>
-                        <button onClick={PlusCountProduct2}>Plus</button>
+
+
+
+                <div>
+                    <p>Total Products:</p>
+                    <hr />
+                    <p>Counter: {TotalProducts}</p>
+                </div>
+
+                <div>
+                    <p>Buttons:</p>
+                    <hr/>
+                    <div className="flex justify-around">
+                        <button onClick={DoNothing}>Do nothing</button>
+                        <button onClick={ClearProducts}>Clear Products</button>
+                        <button onClick={AddProduct}>Add Product</button>
                     </div>
                 </div>
-            </div>
 
 
 
+                <div>
+                    <p>Products (Map from array):</p>
+                    <hr/>
+                    {Products.map(product => (
+                        <p key={product.id}>{product.name}</p>
+                    ))}
+                </div>
+            </div> */}
+
+
+
+
+
+
+
+            {/* ---------- Section 02 */}
             <div>
-                <p>Total Products:</p>
-                <hr />
-                <p>Counter: {TotalProducts}</p>
-            </div>
+                <div>
+                    <h1 className="text-xl text-red-600">Section 02:</h1>
+                    <hr />
+                </div>
 
-            <div>
-                <p>Buttons:</p>
-                <hr/>
-                <div className="flex justify-around">
-                    <button onClick={DoNothing}>Do nothing</button>
-                    <button onClick={ClearProducts}>Clear Products</button>
-                    <button onClick={AddProduct}>Add Product</button>
+                <div>
+                    
                 </div>
             </div>
 
 
 
-            <div>
-                <p>Products (Map from array):</p>
-                <hr/>
-                {Products.map(product => (
-                    <p key={product.id}>{product.name}</p>
-                ))}
-            </div>
 
 
 
+            {/* ---------- Section 02 */}
         </div>
     )
 }
