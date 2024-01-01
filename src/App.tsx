f767af1fadb91c5f148c85d7c9b46e448ef73c7f
Henry Milac <henrymilacprog@gmail.com>
2023-12-31 19:11:14 -0500

    starting again

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import ZustandFernandoHerrera from "./MixerFront/Issues/ManageState/Zustand/ZustandFernandoHerrera/ZustandFernandoHerrera";
-import Z from "./MixerFront/Z/Z";
-
 
 export default function App() {
 
   return (
     <>
-      {/* <ZustandFazt/> */}
-      {/* <ZustandMe/> */}
-      {/* <ZustandFernandoHerrera/> */}
-
-      <Z/>
+   
     </>
   )
 }
--- src/MixerFront/Z/Z.tsx
+++ src/MixerFront/Z/Z.tsx
@@ -1,34 +1,9 @@
-import { useNotesStore } from "./store/notes.store"
+
 
 export default function Z() {
-
-    const notes = useNotesStore(state => state.notes)
-
-    return (
-        <div className="p-5 flex flex-col gap-y-5 bg-gray-900 h-full text-white">
-            <div>
-                <p>Notes:</p>
-                <hr />
-            </div>
-
-            <form>
-                <label htmlFor="">Nota:</label>
-                <input type="text" className="border border-gray-400"
-                />
-                <input type="submit" value="Add" />
-            </form>
-
-            <div>
-                {notes.map(note => (
-                    <div key={note.id} className="flex justify-between">
-                        <p>{note.note}</p>
-                        <div>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
+    return(
+        <div>
+            
         </div>
     )
 }  
