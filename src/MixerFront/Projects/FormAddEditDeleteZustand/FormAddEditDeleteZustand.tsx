409030840ba15506ac3ec0080bfd9b9291948337
Henry Milac <henrymilacprog@gmail.com>
2023-12-31 19:36:35 -0500

    some details

--- src/MixerFront/Projects/FormAddEditDeleteZustand/FormAddEditDeleteZustand.tsx
+++ src/MixerFront/Projects/FormAddEditDeleteZustand/FormAddEditDeleteZustand.tsx
@@ -6,19 +6,21 @@ export default function FormAddEditDeleteZustand() {
 
     return (
         <div className="p-5 flex flex-col gap-y-5 bg-black text-white h-full">
-            <div>
-                <h1>Formulario:</h1>
-                <hr />
-            </div>
 
-            <form className="border border-gray-300 p-3">
+            <form className="border border-gray-300 p-3 flex flex-col gap-y-3">
                 <div>
-                    <label htmlFor="">Note:</label>
-                    <input type="text" className="border border-gray-300"/>
+                    <h1>Formulario:</h1>
+                    <hr />
                 </div>
-                <div>
-                    <label htmlFor="">Category:</label>
-                    <input type="text" className="border border-gray-300"/>
+                <div className="flex">
+                    <div className="">
+                        <label htmlFor="">Note:</label>
+                        <input type="text" className="border border-gray-300"/>
+                    </div>
+                    <div>
+                        <label htmlFor="">Category:</label>
+                        <input type="text" className="border border-gray-300"/>
+                    </div>
                 </div>
                 <input type="submit" value="Add Note" className="border border-gray-300 px-4"/>
             </form>
