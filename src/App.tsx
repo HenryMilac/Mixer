228653507d476e9a24ee3b51c265d99cfb4039f9
Henry Milac <henrymilacprog@gmail.com>
2024-01-08 12:40:33 -0500

    Maquetado & JSON done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Reacti18next from "./MixerFront/Exercises/TranslatePage/react-i18next/Reacti18next";
+import Z from "./MixerFront/Z/Z";
 
 export default function App() {
   return (
     <>
-      <Reacti18next/>
+      {/* <Reacti18next/> */}
+      <Z/>
     </>
   )
 }
---
+++ src/MixerFront/Z/src/components/BtnTranslate.tsx
@@ -0,0 +1,7 @@
+export default function BtnTranslate() {
+    return (
+        <button className="border border-gray-300 py-2 px-10 bg-green-700 text-white rounded-lg uppercase font-bold">
+            Traducir
+        </button>
+    )
+}
