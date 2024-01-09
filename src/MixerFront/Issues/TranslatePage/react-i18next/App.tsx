84e0c494bc7504409bdae8b256ac4d72e56b020e
Henry Milac <henrymilacprog@gmail.com>
2024-01-09 15:30:20 -0500

    Configurate done and consuming a the first data from the JSON

--- src/MixerFront/Issues/TranslatePage/react-i18next/App.tsx
+++ src/MixerFront/Issues/TranslatePage/react-i18next/App.tsx
@@ -1,15 +1,41 @@
+import i18next from 'i18next';
+// Translate files JSON
+import global_en from './src/translations/en/global.json';
 import BtnTranslate from "./src/components/BtnTranslate";
+// Components
 import CardProduct from "./src/components/CardProduct";
+import global_es from './src/translations/es/global.json';
+import { useTranslation } from 'react-i18next';
+
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: 'en',
+    resources: {
+        en: {
+            global: global_en
+        },
+        es: {
+            global: global_es
+        },
+    },
+});
+
+
+
+
 
 export default function App() {
+
+    const [t] = useTranslation('global');
+
     return (
         <div className="p-5 relative pb-24 max-w-2xl mx-auto">
             <div className="mb-7 border-b border-gray-300">
-                <p className="text-2xl font-bold">Products:</p>
+                <p className="text-2xl font-bold">{t("titleApp")}:</p>
             </div>
             
             <div className="flex flex-col gap-y-5">
-                <CardProduct/>
                 <CardProduct/>
                 <CardProduct/>
             </div>
--- src/MixerFront/Issues/TranslatePage/react-i18next/Reacti18next.tsx
+++ src/MixerFront/Issues/TranslatePage/react-i18next/Reacti18next.tsx
@@ -1,9 +1,16 @@
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
 import App from "./App";
+
+
+
+
 
 export default function Reacti18next() {
     return (
-        <div>
+        <I18nextProvider i18n={i18next}>
             <App/>
-        </div>
+        </I18nextProvider>
     )
 }
