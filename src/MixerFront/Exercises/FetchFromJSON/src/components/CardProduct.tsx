956647af39f920e3a45b2068c289ab481eb3aeb6
Henry Milac <henrymilacprog@gmail.com>
2024-01-10 21:03:42 -0500

    FetchFromJSON: consumming is done

--- src/MixerFront/Exercises/FetchFromJSON/src/components/CardProduct.tsx
+++ src/MixerFront/Exercises/FetchFromJSON/src/components/CardProduct.tsx
@@ -1,34 +1,7 @@
-import CardSale from "./CardSale";
-
-interface CardProductProps {
-  productData: ProductData;
-}
-interface ProductData {
-  id: number;
-  nameProduct: string;
-  categoryProduct: string;
-  salesTitle: string;
-  sales: {
-    id: number;
-    dateTitle: string;
-    date: string;
-    total: string;
-    "colors&units": { color: string; units: string; }[];
-  }[];
-}
-
-
-export default function CardProduct({ productData }: CardProductProps) {
-  const salesData = productData.sales;
-
+export default function CardProduct() {
   return (
-    <div key={productData.id}>
-      <p>{productData.nameProduct}</p>
-      <p>{productData.categoryProduct}</p>
-      <p>{productData.salesTitle}:</p>
-      {salesData.map((saleData) => (
-        <CardSale key={saleData.id} saleData={saleData} />
-      ))}
+    <div>
+      
     </div>
-  );
+  )
 }
--- src/MixerFront/Exercises/FetchFromJSON/src/components/CardSale.tsx
+++ src/MixerFront/Exercises/FetchFromJSON/src/components/CardSale.tsx
@@ -1,15 +1,7 @@
-export default function CardSale({ saleData }) {
+export default function CardSale() {
   return (
-    <div key={saleData.id} className='ml-10'>
-    <p>{saleData.dateTitle}</p>
-    <p>{saleData.date}</p>
-    <p>S/. {saleData.total}</p>
-    {saleData["colors&units"].map(colorUnit => (
-        <div key={colorUnit.color} className='ml-10'>
-            <p>{colorUnit.color}</p>
-            <p>{colorUnit.units} units</p>
-        </div>
-    ))}
-</div>
+    <div>
+      
+    </div>
   )
 }
