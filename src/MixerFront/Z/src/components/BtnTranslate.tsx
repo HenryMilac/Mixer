52c33ee488c853576f218d935ee738caa7545ae5
Henry Milac <henrymilacprog@gmail.com>
2024-01-10 20:09:37 -0500

    trial: Z is ready

--- src/MixerFront/Z/src/components/BtnTranslate.tsx
+++
@@ -1,7 +0,0 @@
-export default function BtnTranslate() {
-    return (
-        <button className="border border-gray-300 py-2 px-10 bg-green-700 text-white rounded-lg uppercase font-bold">
-            Traducir
-        </button>
-    )
-}
