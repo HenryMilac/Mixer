34b0d42c316a1cee10e28498d2c84d82ecc1f38b
Henry Milac <henrymilacprog@gmail.com>
2024-01-12 18:31:56 -0500

    FetchFromJSON: remastering

---
+++ src/MixerFront/Exercises/FetchFromJSON/src/components/CardSell.tsx
@@ -0,0 +1,10 @@
+export default function CardSell({sells}:any) {
+
+    return (
+        <>
+            {sells.map(sell => (
+                <p key={sell.id} className="border p-2">{sell.product}</p>
+            ))}
+        </>
+    )
+}
---
+++ src/MixerFront/Exercises/FetchFromJSON/src/components/CardStore.tsx
@@ -0,0 +1,21 @@
+import CardSeller from "./CardSeller";
+
+export default function CardStore({stores}:any) {
+
+    return (
+        <>
+            {stores.map(store => (
+                <div key={store.id} className="border p-2">
+                    <p>{store.city}</p>
+                    <hr />
+                    <p>Sellers:</p>
+                    <div className="flex flex-col gap-y-2">
+                        <CardSeller
+                            sellers={store.sellers}
+                        />
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
