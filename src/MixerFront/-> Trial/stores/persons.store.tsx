a73393d5a072e68bba62c2eda079743db9fa1b49
Henry Milac <henrymilacprog@gmail.com>
2024-01-17 17:57:54 -0500

    Trial: start

--- src/MixerFront/-> Trial/stores/persons.store.tsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const usePersonsStore = create((set) => ({
-    name: 'Henry',
-    age: 22,
-    plus: () => set(state => ({ age: state.age + 1 })),
-    rest: () => set(state => ({ age: state.age - 1 }))
-}))
