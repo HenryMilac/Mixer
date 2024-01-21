518b8e096f1b37f363688ad4e5f7bb39a7ef7142
Henry Milac <henrymilacprog@gmail.com>
2024-01-21 13:21:58 -0500

    Trial: Starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import IconMenuToNavbar from "./MixerFront/Components/IconMenuShowNavbar";
-import SwipeEditDelete from "./MixerFront/Components/SwipeEditDelete";
-import FetchFromJSON from "./MixerFront/Exercises/Fetching/FetchFromJSON/FetchFromJSON";
-import FormNewEditDeleteUseState from "./MixerFront/Exercises/Forms/FormNewEditDeleteUseState/FormNewEditDeleteUseState";
-import FormNewEditDeleteZustand from "./MixerFront/Exercises/Forms/FormNewEditDeleteZustand/FormNewEditDeleteZustand";
+import Trial from "./MixerFront/-> Trial/Trial";
 
 
 export default function App() { return (<div>
-{/* <Trial/> */}
-{/* <FormNewEditDeleteUseState/> */}
-{/* <FormNewEditDeleteZustand/> */}
-{/* <FetchFromJSON/> */}
-{/* <IconMenuToNavbar/> */}
-<SwipeEditDelete/>
+<Trial/>
+
 
 </div>)}
