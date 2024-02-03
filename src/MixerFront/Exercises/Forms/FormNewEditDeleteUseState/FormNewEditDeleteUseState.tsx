de512ecca9ddd610b14cfb0aec69d89e86f72a09
Henry Milac <henrymilacprog@gmail.com>
2024-02-03 18:14:01 -0500

    FormNewEditDeleteUseState: Done with types

--- src/MixerFront/Exercises/Forms/FormNewEditDeleteUseState/FormNewEditDeleteUseState.tsx
+++ src/MixerFront/Exercises/Forms/FormNewEditDeleteUseState/FormNewEditDeleteUseState.tsx
@@ -1,37 +1,50 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+
+interface Person {
+    id: number;
+    name: string;
+    age: number;
+}
 
 export default function FormNewEditDeleteUseState() {
 
-    const [persons, setPersons] = useState([])
-    const [person, setPerson] = useState({})
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const [persons, setPersons] = useState<Person[]>([])
+    const [person, setPerson] = useState<Person>({ id: 0, name: '', age: 0 })
+    const [name, setName] = useState<string>('')
+    const [age, setAge] = useState<number>(0)
     
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        const personObject = {
-            name, 
+        const personObject: Person = {
+            id: person.id,
+            name,
             age
         }
 
         if(person.id){
             personObject.id = person.id
-            const personUpdate = persons.map(personState => 
+            const personUpdate = persons.map(personState =>
                 personState.id === person.id ? personObject : personState
             )
             setPersons(personUpdate)
-            setPerson({})
+            setPerson({ id: 0, name: '', age: 0 })
         }else {
             personObject.id = Date.now()
             setPersons([...persons, personObject])
         }
 
         setName('')
-        setAge('')
+        setAge(0)
+
+        if(inputRef.current){
+            inputRef.current.focus()
+        }
     }
 
-    const handleDelete = id => {
+    const handleDelete = (id: number) => {
         const confirmDelete = confirm('Seguro que quiere eliminar?')
         if(confirmDelete){
             const personsUpdate = persons.filter(person => person.id !== id)
@@ -47,28 +60,24 @@ export default function FormNewEditDeleteUseState() {
     }, [person])
 
     return (
-        <div className="max-w-2xl mx-auto py-10 flex flex-col gap-y-10">
-            <form onSubmit={handleSubmit} className="border border-white p-3 flex flex-col gap-y-5">
-                <h1 className="text-2xl border-b border-white">Form:</h1>
-                <div className="flex flex-col">
-                    <label htmlFor="">Name: </label>
-                    <input type="text" placeholder="Name" className="border text-black"
-                        value={name}
-                        onChange={(e) => {setName(e.target.value)}}
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="">Age: </label>
-                    <input type="number" placeholder="Age" className="border text-black"
-                        value={age}
-                        onChange={(e) => {setAge(e.target.value)}}
-                    />
-                </div>
+        <div className="max-w-2xl mx-auto p-5 flex flex-col gap-y-5">
+            <form onSubmit={handleSubmit} className="flex flex-col"> 
+                <h1 className="mb-2">Form:<hr/></h1>
+                <input type="text" placeholder="Name" className="text-black"
+                    value={name}
+                    onChange={(e) => {setName(e.target.value)}}
+                    ref={inputRef}
+                />
+                <input type="number" placeholder="Age" className="text-black"
+                    value={age}
+                    onChange={(e) => {setAge(Number(e.target.value))}}
+                />
+                
                 <input type="submit" value={person.id ? 'Edit': 'Send'} className="border py-1 px-5 cursor-pointer"/>
             </form>
             
-            <div className="flex flex-col gap-y-5">
-                <h1 className="text-2xl border-b border-white">Result:</h1>
+            <div>
+                <h1>Result:<hr/></h1>
                 {persons.map((person) => (
                     <div className="flex justify-between" key={person.id}>
                         <p>{person.name}</p>
