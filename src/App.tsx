7cc0e549e353883ffb33dd4217d8ff943c37f89e
Henry Milac <henrymilacprog@gmail.com>
2024-02-05 13:08:53 -0500

    General: Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import Trial from "./MixerFront/-> Trial/Trial";
-import FormNewEditDeleteUseState from "./MixerFront/Exercises/Forms/FormNewEditDeleteUseState/FormNewEditDeleteUseState";
-import FormNewEditDeleteZustand from "./MixerFront/Exercises/Forms/FormNewEditDeleteZustand/FormNewEditDeleteZustand";
-import ControlPresupuesto from "./MixerFront/Projects/ControlPresupuesto/ControlPresupuesto";
+import Trial from "./MixerFront/Trial/Trial";
 
 
 export default function App() { return (<div>
 
 <Trial/>
-
-{/* <FormNewEditDeleteUseState/> */}
-{/* <FormNewEditDeleteZustand/> */}
-{/* <ControlPresupuesto/> */}
-
-
 
 
 
