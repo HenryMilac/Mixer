777f252c4c695490c63168e6db8c5a0635aa2983
Henry Milac <henrymilacprog@gmail.com>
2024-03-10 14:27:30 -0500

    refactor: some details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Trial from './trial/Trial'
 import DragCardsColumnZustand from './exercises/DragCardsColumnZustand/DragCardColumnZustand'
 import FormNewEditDeleteZustand from './exercises/FormNewEditDeleteZustand/FormNewEditDeleteZustand'
+import FormNewEditDeleteUseState from './exercises/FormNewEditDeleteUseState/FormNewEditDeleteUseState'
 import ReuseComponent from './exercises/ReuseComponent/ReuseComponent'
 
 
@@ -13,10 +14,11 @@ import ReuseComponent from './exercises/ReuseComponent/ReuseComponent'
 export default function App() {
     return (
         <div className="max-w-2xl mx-auto p-7 border h-screen my-10 rounded-2xl">
-            <Trial/>
+            {/* <Trial/> */}
 
             {/* <DragCardsColumnZustand/> */}
             {/* <FormNewEditDeleteZustand/> */}
+            <FormNewEditDeleteUseState/>
             {/* <ReuseComponent/> */}
         </div>
     )
--- client/src/exercises/FormNewEditDeleteUseState/FormNewEditDeleteUseState.tsx
+++ client/src/exercises/FormNewEditDeleteUseState/FormNewEditDeleteUseState.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react"
 interface Person {
     id: number;
     name: string;
-    age: string;
+    lastName: string;
 }
 
 export default function FormNewEditDeleteUseState() {
@@ -11,83 +11,84 @@ export default function FormNewEditDeleteUseState() {
     const inputRef = useRef<HTMLInputElement>(null)
 
     const [persons, setPersons] = useState<Person[]>([])
-    const [person, setPerson] = useState<Person>({ id: 0, name: "", age: "" })
+    const [person, setPerson] = useState<Partial<Person>>({})
     const [name, setName] = useState<string>('')
-    const [age, setAge] = useState<string>('')
+    const [lastName, setLastName] = useState<string>('')
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-
-        const personObject: Person = {
-            id: person.id,
-            name,
-            age
+        const newPerson = {
+            name: name,
+            lastName: lastName
         }
-
-        if (person.id) {
-            const personUpdate = persons.map(personEditing =>
-                personEditing.id === person.id ? personObject : personEditing
-            )
-            setPersons(personUpdate)
-            setPerson({ id: 0, name: "", age: "" })
-        } else {
-            personObject.id = Date.now()
-            setPersons([...persons, personObject])
+        if(person.id){
+            const personsEditing = persons.map(personEdit => {
+                if(personEdit.id === person.id){
+                    return {
+                        ...personEdit,
+                        name: name,
+                        lastName: lastName
+                    }
+                }
+                return personEdit
+            })
+            setPersons(personsEditing)
+        }else {
+            const completeNewPerson: Person = {
+                ...newPerson,
+                id: Date.now(),
+            } as Person;
+            setPersons([...persons, completeNewPerson]);
         }
-
+        setPerson({})
         setName('')
-        setAge('')
-
-        if (inputRef.current) {
-            inputRef.current.focus()
+        setLastName('')
+        if(inputRef.current) {
+            inputRef.current.focus();
         }
     }
 
     const handleDelete = (id: number) => {
-        const confirmDelete = window.confirm('Seguro que quiere eliminar?')
-        if (confirmDelete) {
-            const personsUpdate = persons.filter(person => person.id !== id)
-            setPersons(personsUpdate)
-        }
+        const personsFiltered = persons.filter(person => person.id !== id)
+        setPersons(personsFiltered)
     }
 
     useEffect(() => {
-        if (Object.keys(person).length > 0) {
-            setName(person.name)
-            setAge(person.age)
+        if (person.id !== undefined) {
+            setName(person.name || '');
+            setLastName(person.lastName || '');
         }
     }, [person])
 
     return (
-        <div className="max-w-2xl mx-auto p-5 flex flex-col gap-y-5">
-            <form onSubmit={handleSubmit} className="flex flex-col">
-                <h1 className="mb-2">Form:<hr /></h1>
+        <>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Name" className="text-black"
                     value={name}
                     onChange={(e) => { setName(e.target.value) }}
                     ref={inputRef}
                 />
-                <input type="number" placeholder="Age" className="text-black"
-                    value={age}
-                    onChange={(e) => { setAge(e.target.value) }}
+                <input type="text" placeholder="Last Name" className="text-black"
+                    value={lastName}
+                    onChange={(e) => { setLastName(e.target.value) }}
                 />
 
-                <input type="submit" value={person.id ? 'Edit' : 'Send'} className="border py-1 px-5 cursor-pointer" />
+                <input type="submit" value={person.id ? 'Edit' : 'Add'}/>
             </form>
 
             <div>
-                <h1>Result:<hr /></h1>
+                <h1>Persons:<hr /></h1>
                 {persons.map((person) => (
                     <div className="flex justify-between" key={person.id}>
                         <p>{person.name}</p>
-                        <p>{person.age}</p>
+                        <p>{person.lastName}</p>
                         <div className="">
-                            <button onClick={() => setPerson(person)} className="mr-5">Edit</button>
+                            <button onClick={() => setPerson(person)}>Edit</button>
                             <button onClick={() => handleDelete(person.id)}>Delete</button>
                         </div>
                     </div>
                 ))}
             </div>
-        </div>
+        </>
     )
 }
