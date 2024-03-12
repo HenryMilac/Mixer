7fbb8d5af7c063aefbc15a722c708c936ea781ab
Henry Milac <henrymilacprog@gmail.com>
2024-03-12 13:45:01 -0500

    fix: files in order

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Trial from './trial/Trial'
 import ExlDragCardsColumnZustand from './components/ExldragCardsColumnZustand/ExldragCardColumnZustand'
-
+import ExlFormReactHookForm from './components/ExlFormReactHookForm/ExlFormReactHookForm'
 
 
 
@@ -13,10 +13,8 @@ export default function App() {
         <div className="max-w-2xl mx-auto p-7 border h-screen my-10 rounded-2xl">
             {/* <Trial/> */}
 
-            <ExlDragCardsColumnZustand/>
-            {/* <FormNewEditDeleteZustand/> */}
-            {/* <FormNewEditDeleteUseState/> */}
-            {/* <ReuseComponent/> */}
+            {/* <ExlDragCardsColumnZustand/> */}
+            <ExlFormReactHookForm/>
         </div>
     )
 }
