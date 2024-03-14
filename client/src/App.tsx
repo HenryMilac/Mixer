f5618e82792df4c0c8aec50080f24e5ef8eee885
Henry Milac <henrymilacprog@gmail.com>
2024-03-14 08:21:29 -0500

    feat: Form React Hook Form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 import Trial from './trial/Trial.jsx'
 import ExlDragCardsColumnZustand from './components/ExldragCardsColumnZustand/ExldragCardColumnZustand'
 import ExlFormReactHookForm from './components/ExlFormReactHookForm/ExlFormReactHookForm.jsx'
@@ -12,10 +13,10 @@ export default function App() {
     return (
         <div className="m-5">
             <div className='max-w-2xl p-5 border border-gray-500 rounded-xl mx-auto'>
-                {/* <Trial/> */}
+                <Trial/>
 
                 {/* <ExlDragCardsColumnZustand/> */}
-                <ExlFormReactHookForm/>
+                {/* <ExlFormReactHookForm/> */}
             </div>
         </div>
     )
