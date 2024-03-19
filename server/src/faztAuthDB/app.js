c3c58f590d9db0d62e2d12d380180cf0cb48d070
Henry Milac <henrymilacprog@gmail.com>
2024-03-19 06:27:45 -0500

    feat: fazt auth project working

--- server/src/faztAuthDB/app.js
+++ server/src/faztAuthDB/app.js
@@ -1,7 +1,8 @@
-import express, { json } from "express";
+import express from "express";
 import morgan from 'morgan';
 
 const app = express();
+app.use(express.json());
 app.use(morgan('dev'));
-app.use(json());
+
 export default app;
