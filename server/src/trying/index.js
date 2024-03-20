3858c84ddb6917159c063180c5f4c079e21786ad
Henry Milac <henrymilacprog@gmail.com>
2024-03-20 06:12:47 -0500

    feat: fazt auth project working

--- server/src/trying/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRoutes from './routes/auth.routes.js'
-import { connectDB } from "./db.js";
-const app = express()
-
-app.listen(3000)
-app.use(express.json())
-app.use(authRoutes)
-connectDB()
--- server/src/trying/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { register } from "../controllers/auth.controllers.js";
-
-const routes = Router()
-
-routes.post('/register', register)
-
-export default routes
