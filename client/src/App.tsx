f0172b7dd3826067c943774a3de9e9c683670e2d
Henry Milac <henrymilacprog@gmail.com>
2024-05-09 12:35:57 -0500

    mixer/exercise/SearchCarSlow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,4 +20,4 @@ export default function App() {
             </div>
         </div>
     )
-}11
+}
--- client/src/trial/Trial.jsx
+++ client/src/trial/Trial.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Trial() {
+function Trial() {
   return (
     <div>
       
     </div>
   )
 }
+
+export default Trial
