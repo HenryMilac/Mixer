43e78b1e38c601b116ab7e316d450602df3f2865
Henry Milac <henrymilacprog@gmail.com>
2024-05-17 12:35:22 -0500

    ReactNat/Project/Aora/feat: First advanced

---
+++ clientMob/app/index.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import  { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style='auto'/>
+      <Text>Inicio</Text>
+    </View>
+  )
+}
