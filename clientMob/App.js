7f4f5d5597194a39a2581c6b11341d49a61890ba
henryramirezdatamatrix <henrymilacprog@gmail.com>
2024-07-07 16:51:28 -0500

    starting

---
+++ clientMob/App.js
@@ -0,0 +1,27 @@
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import PrlFirebaseLoginFbGoog from './mixerMobile/PrlFirebaseLoginFbGoog/PrlFirebaseLoginFbGoog';
+
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <PrlFirebaseLoginFbGoog/>
+
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 50
+  },
+});
