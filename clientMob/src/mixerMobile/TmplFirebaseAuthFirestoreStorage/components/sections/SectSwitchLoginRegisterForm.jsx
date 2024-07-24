10116d978e352755cf704a5d0e9ba41211af36b2
henryramirezdatamatrix <henrymilacprog@gmail.com>
2024-07-24 11:19:02 -0500

    ExlFirebaseImageUser/ fix: login form when you exit form form

--- clientMob/src/mixerMobile/TmplFirebaseAuthFirestoreStorage/components/sections/SectSwitchLoginRegisterForm.jsx
+++ clientMob/src/mixerMobile/TmplFirebaseAuthFirestoreStorage/components/sections/SectSwitchLoginRegisterForm.jsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { useUserStore } from '../../store/user.store';
 
 export default function SectSwitchLoginRegisterForm({ textMessage, textLink, link}) {
     const navigation = useNavigation()
-    
+    const setUser_email = useUserStore(state => state.setUser_email);
+    const setUser_password = useUserStore(state => state.setUser_password);
+
+    const handlePress = () => {
+        setUser_email('')
+        setUser_password('')
+        navigation.navigate(link);
+    }
     return (
         <View style={styles.loginContainer}>
             <Text>{textMessage}</Text>
-            <TouchableOpacity onPress={() => navigation.navigate(link)}>
+            <TouchableOpacity onPress={handlePress}>
                 <Text>{textLink}</Text>
             </TouchableOpacity>
         </View>
